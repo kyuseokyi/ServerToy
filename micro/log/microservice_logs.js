413b26f1d173861011b06c527453b6b421fd2012
kyuseokyi <kyuseok.y@gmail.com>
2020-02-06 12:29:02 +0900

    add microservice_logs.js

---
+++ micro/log/microservice_logs.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const cluster = require('cluster');
+const server = require('../server');
+class logs extends server {
+    constructor() {
+        super("logs", process.argv[2] ? Number(process.argv[2]) : 9040, ["POST/logs"]);
+
+        this.connectToDistributor("127.0.0.1", 9000, (data) => {
+            console.log("Distributor Notification", data);
+        });
+    };
+
+    onRead(socket, data) {
+        const sz = new Date().toLocaleString() + '\t' + socket.remoteAddress + '\t' + socket.remotePort + '\t' + JSON.stringify(data) + '\n';
+        console.log(sz);
+    }
+}
+
+if (cluster.isMaster) {
+    cluster.fork();
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log('worker ${worker.process.pid} died');
+        cluster.fork();
+    });
+} else {
+    new logs();
+}
